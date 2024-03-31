44a142506b8b35fd28b854111e7a37c783901da5
Diana Cismaru <diana.cismaru@stud.acs.upb.ro>
2024-03-31 22:53:16 +0300

    Process data from csv file

--- app/data_ingestor.py
+++ app/data_ingestor.py
@@ -3,8 +3,6 @@ import json
 
 class DataIngestor:
     def __init__(self, csv_path: str):
-        # TODO: Read csv from csv_path
-
         self.questions_best_is_min = [
             'Percent of adults aged 18 years and older who have an overweight classification',
             'Percent of adults aged 18 years and older who have obesity',
@@ -19,3 +17,30 @@ class DataIngestor:
             'Percent of adults who achieve at least 300 minutes a week of moderate-intensity aerobic physical activity or 150 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
             'Percent of adults who engage in muscle-strengthening activities on 2 or more days a week',
         ]
+
+        self.data = {}
+        self.index_dict = {}
+
+        with open (csv_path, 'r') as f:
+            first_line = True
+            for line in f.readlines():
+                if first_line:
+                    self.columns = line.strip().split(',')
+                    for col in range(len(self.columns)):
+                        self.index_dict[self.columns[col]] = col
+                    first_line = False
+                else:
+                    curr_line = line.strip().split(',')
+
+                    question = curr_line[8]
+                    if question not in self.data:
+                        self.data[question] = {}
+                    
+                    location = curr_line[4]
+                    if location not in self.data[question]:
+                        self.data[question][location] = []
+                    
+                    self.data[question][location].append(curr_line)
+
+        
+        
